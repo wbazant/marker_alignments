7bf03eeed1cf726cfe888b865e5aed4b4ffcab1f
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-07-28 11:52:13 -0400

    Fix test

--- tests/read_alignments.py
+++ tests/read_alignments.py
@@ -16,7 +16,7 @@ class ReadAlignments(unittest.TestCase):
         marker_to_taxon_id = { "taxon_1:marker_1" : "id_1"}
         alignment_store = read_alignments(sam, None, pattern_taxon, pattern_marker, marker_to_taxon_id)
         content = [t for t in alignment_store.query('select * from alignment')]
-        self.assertEqual(content, [('taxon_1', 'marker_1', 'query_id', 1.0, 0.141026), ('taxon_2', 'marker_2', 'second_query_id', 0.973511, 0.02327), ('taxon_2', 'marker_3', 'third_query_id', 0.973511, 0.074),  ('taxon_2', 'marker_3', 'fourth_query_id', 0.973511, 0.074)])
+        self.assertEqual(content, [('id_1:taxon_1', 'marker_1', 'query_id', 1.0, 0.141026), ('taxon_2', 'marker_2', 'second_query_id', 0.973511, 0.02327), ('taxon_2', 'marker_3', 'third_query_id', 0.973511, 0.074),  ('taxon_2', 'marker_3', 'fourth_query_id', 0.973511, 0.074)])
 
 
 if __name__ == '__main__':
